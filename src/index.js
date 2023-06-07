355e50d8492644bf3c82680340e5428c5eb1f5b3
IRINA IVANOVA <iivanova@mmtr.ru>
2023-06-07 16:38:01 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then((result) => console.log(result));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <ApolloProvider client={client}>
+      <React.StrictMode>
+      <App />
+    </React.StrictMode></ApolloProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
